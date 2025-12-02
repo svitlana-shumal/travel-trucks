0230e4df42f0cc7f95a61c406bbb53153af2e4f4
Svitlana Shumal <svetlankachajka@gmail.com>
2025-12-02 15:54:55 +0200

    added files header and hero

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,23 +1,27 @@
+'use client';
+
 import Link from 'next/link';
 import css from './Header.module.css';
 
 export default function Header() {
   return (
-    <header>
+    <header className={css.header}>
       <div className={css.container}>
         <Link href="/">
           <svg width={136} height={16}>
             <use href="/symbol-defs.svg#icon-Logo"></use>
           </svg>
         </Link>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/catalog">Catalog</Link>
-          </li>
-        </ul>
+        <nav aria-label="Main Navigation">
+          <ul className={css.navigation}>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/catalog">Catalog</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,5 +1,16 @@
+import Link from 'next/link';
 import css from './Hero.module.css';
 
 export default function Hero() {
-  return <div></div>;
+  return (
+    <section className={css.hero}>
+      <div className={css.heroDiv}>
+        <h1 className={css.title}>Campers of your dreams</h1>
+        <p className={css.desc}>You can find everything you want in our catalog</p>
+        <Link href="/catalog">
+          <button className={css.btn}>View Now</button>
+        </Link>
+      </div>
+    </section>
+  );
 }
