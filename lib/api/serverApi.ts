1118a37960109de11b5ddeec644ab6ab4859ad05
Svitlana Shumal <svetlankachajka@gmail.com>
2025-12-02 20:04:45 +0200

    added types and lib/api

---
+++ lib/api/serverApi.ts
@@ -0,0 +1,11 @@
+import { nextServer } from './api';
+
+export const getCampers = async (filters?: Record<string, string | number | boolean>) => {
+  const { data } = await nextServer.get('/campers', { params: filters });
+  return data;
+};
+
+export const getCampersById = async (id: string) => {
+  const { data } = await nextServer.get('/campers/${id}');
+  return data;
+};
